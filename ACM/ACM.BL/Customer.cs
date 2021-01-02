7c4f37c0cf1a4939cfbf92a0445229809b1f8cb2
FGTO <fongqto@gmail.com>
2021-01-02 21:48:14 +0100

    First upload
    First upload from this project

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerID { get; private set; }
+    
+        public string EmailAdress { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + "," + FirstName;
+            }
+        }
+        public string VariableName;
+
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+    }
+}
