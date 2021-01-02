9da2c74ed61a685f234f562c93814d0d0a53fe5b
FGTO <fongqto@gmail.com>
2021-01-02 23:01:32 +0100

    Add a test
    Added a xUnit test for full name validation.

--- ACM/Tests/ACM.BLtest/CustomerTest.cs
+++ ACM/Tests/ACM.BLtest/CustomerTest.cs
@@ -1,14 +1,27 @@
 using System;
 using Xunit;
+using ACM.BL;
 
 namespace ACM.BLtest
 {
-    public class UnitTest1
+    public class CustomerTest
     {
         [Fact]
-        public void Test1()
+        public void FullNameTestValid()
         {
+            // -- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
 
+            // -- Act
+            string actual = customer.FullName;
+
+            // -- Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
